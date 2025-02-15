380cc2ac80c02ffde7599c1db49ac56408f719ee
nivethan-nirosh <nirosh.26d@gmail.com>
2025-02-15 17:31:18 +0530

    PrimeNG Component Integration

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ToolbarModule } from 'primeng/toolbar';
+import { ButtonModule } from 'primeng/button';
+import { SplitButtonModule } from 'primeng/splitbutton';
+import { InputTextModule } from 'primeng/inputtext';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { DrawerModule } from 'primeng/drawer';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    ToolbarModule,
+    ButtonModule,
+    SplitButtonModule,
+    InputTextModule,
+    IconFieldModule,
+    InputIconModule,
+    DrawerModule
+  ],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrls: ['./navbar.component.scss'] // ✅ Corrected from "styleUrl"
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  items: MenuItem[] = [];
+  visible: boolean = false;
 
+  ngOnInit() {
+    this.items = [
+      { label: 'Update', icon: 'pi pi-refresh' },
+      { label: 'Delete', icon: 'pi pi-times' }
+    ];
+  }
 }
