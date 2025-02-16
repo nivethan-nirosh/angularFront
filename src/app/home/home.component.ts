2f72bbe2d4fccb39ff11fd5b3eaf010e1f08232d
nivethan-nirosh <nirosh.26d@gmail.com>
2025-02-16 09:59:08 +0530

    Interpolation Practised, Code in Theory.txt(Component.ts&component.html)(home)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { FooterComponent } from '../footer/footer.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent, FooterComponent],
+  imports: [NavbarComponent, FooterComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
 
+ 
 }
