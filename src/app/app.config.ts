c31f6baa57525844c046833c3b2281408971a2b3
nivethan-nirosh <nirosh.26d@gmail.com>
2025-02-16 15:01:29 +0530

    API intergration with Text Interpolation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,11 +5,13 @@ import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes),
     provideAnimationsAsync(),
+    provideHttpClient(),
     providePrimeNG({
         theme: {
             preset: Aura
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { FooterComponent } from '../footer/footer.component';
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { CommonService } from '../common.service';
 
 @Component({
   selector: 'app-home',
@@ -11,6 +13,44 @@ import { CommonModule } from '@angular/common';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  fruitData: any;
+
+  // fruitData = [
+  //   {
+  //     id: 1,
+  //     name: "Apple",
+  //     price: 120
+  //   },
+  //   {
+  //     id: 2,
+  //     name: "Orange",
+  //     price: 110
+  //   },
+  //   {
+  //     id: 3,
+  //     name: "Mango",
+  //     price: 90
+  //   }
+  // ]
+  constructor(private http: HttpClient, private commonService: CommonService){
+    // this.http.get('http://localhost:8080/api/fruits').subscribe({
+    //   next: (data) => {
+    //     console.log(data);
+    //     this.fruitData = data;
+    //   }
+    // })
+
+  }
+  ngOnInit(){
+    this.commonService.getFruits().subscribe({
+      next: (data) =>
+      {
+        console.log(data);
+        this.fruitData = data;
+      }
+    })
+
+  }
 
  
 }
