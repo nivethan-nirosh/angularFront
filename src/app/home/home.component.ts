9fb9a97df83d58af58d97ef19992047dc4e68c2f
nivethan-nirosh <nirosh.26d@gmail.com>
2025-02-17 15:24:21 +0530

    Property&Attribute Binding - Text File Created

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { FooterComponent } from '../footer/footer.component';
 import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
-import { CommonService } from '../common.service';
 
 @Component({
   selector: 'app-home',
@@ -13,44 +11,6 @@ import { CommonService } from '../common.service';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-  fruitData: any;
 
-  // fruitData = [
-  //   {
-  //     id: 1,
-  //     name: "Apple",
-  //     price: 120
-  //   },
-  //   {
-  //     id: 2,
-  //     name: "Orange",
-  //     price: 110
-  //   },
-  //   {
-  //     id: 3,
-  //     name: "Mango",
-  //     price: 90
-  //   }
-  // ]
-  constructor(private http: HttpClient, private commonService: CommonService){
-    // this.http.get('http://localhost:8080/api/fruits').subscribe({
-    //   next: (data) => {
-    //     console.log(data);
-    //     this.fruitData = data;
-    //   }
-    // })
 
-  }
-  ngOnInit(){
-    this.commonService.getFruits().subscribe({
-      next: (data) =>
-      {
-        console.log(data);
-        this.fruitData = data;
-      }
-    })
-
-  }
-
- 
 }
