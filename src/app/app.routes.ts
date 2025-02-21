e00f9ad1b196df9adf5403aebdb1f57623547064
nivethan-nirosh <nirosh.26d@gmail.com>
2025-02-22 02:07:41 +0530

    For

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './home/home.component';
 import { EventComponent } from './event/event.component';
 import { TwoWayDataComponent } from './two-way-data/two-way-data.component';
 import { ParentComponent } from './parent/parent.component';
+import { ControlFlowComponent } from './control-flow/control-flow.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'event', component: EventComponent},
     {path: 'two-way-data', component: TwoWayDataComponent},
-    {path: 'parent', component: ParentComponent}
+    {path: 'parent', component: ParentComponent},
+    {path: 'control', component: ControlFlowComponent}
 ];
