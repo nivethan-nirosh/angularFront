958a595c1a222979226b71ea3eef9dcbcd83904a
nivethan-nirosh <nirosh.26d@gmail.com>
2025-02-23 00:42:06 +0530

    View Child Component Manupulation

--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-student',
@@ -7,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './student.component.scss'
 })
 export class StudentComponent {
+  name: string = 'Nirosh';
+  sayGoodMorningToTeacher(){
+    console.log('Hiii from student component');
+  }
+  @Input() stuItem: number = 6;
 
 }
--- src/app/teacher/teacher.component.ts
+++ src/app/teacher/teacher.component.ts
@@ -1,9 +1,10 @@
 import { Component, ElementRef, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { StudentComponent } from '../student/student.component';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-teacher',
-  imports: [StudentComponent],
+  imports: [StudentComponent, InputTextModule],
   templateUrl: './teacher.component.html',
   styleUrl: './teacher.component.scss'
 })
@@ -13,6 +14,10 @@ export class TeacherComponent {
   @ViewChild('teacherCheck') teacherChkIn: ElementRef | undefined
   //Have a Look on it
   @ViewChildren('teacherButton') teacherButtonCom : QueryList<ElementRef> | undefined
+  @ViewChild(StudentComponent) studentCom : StudentComponent | undefined
+  @ViewChildren(StudentComponent) studentComs : QueryList<ElementRef> | undefined
+
+  @ViewChild('primeInput') inputTextCom: ElementRef| undefined
 
   //Life cycle hook
   ngAfterViewInit(){
@@ -28,11 +33,18 @@ export class TeacherComponent {
     this.teacherButtonCom!.forEach((item) => {
       console.log(item.nativeElement.textContent);
     })
+    
+    this.studentCom?.sayGoodMorningToTeacher();
+    console.log(this.studentCom?.name);
 
+    console.log(this.inputTextCom)
   }
 
   ngAfterViewChecked(){
     console.log('Changes are detected');
+    console.log(this.inputTextCom?.nativeElement.value);
   }
+
+
 
 }
