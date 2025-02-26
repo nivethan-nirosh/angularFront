04e9323526463578cf193f432aa01a3bd3e1e5c5
nivethan-nirosh <nirosh.26d@gmail.com>
2025-02-26 13:50:37 +0530

    Forms Validation

--- src/app/form-group/form-group.component.ts
+++ src/app/form-group/form-group.component.ts
@@ -1,41 +1,62 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { InputTextModule } from 'primeng/inputtext';
 import { TextareaModule } from 'primeng/textarea';
 import { InputNumberModule } from 'primeng/inputnumber';
 import { CheckboxModule } from 'primeng/checkbox';
 import { ButtonModule } from 'primeng/button';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-form-group',
-  imports: [ReactiveFormsModule, InputTextModule, TextareaModule, InputNumberModule, CheckboxModule, ButtonModule],
+  imports: [ReactiveFormsModule, InputTextModule, TextareaModule, InputNumberModule, CheckboxModule, ButtonModule, CommonModule],
   templateUrl: './form-group.component.html',
   styleUrl: './form-group.component.scss'
 })
 export class FormGroupComponent {
   registerForm!: FormGroup;
 
-  constructor(private fb: FormBuilder){
+  constructor(private fb: FormBuilder) {
 
   }
 
-  ngOnInit(){
+  ngOnInit() {
     this.initializeForm();
 
   }
 
-  initializeForm(){
+  initializeForm() {
     this.registerForm = this.fb.group({
-      username : [''],
-      description : [''],
-      phoneno : [],
-      term : [true]
+      username: ['',
+        [
+          Validators.required,
+          Validators.minLength(6),
+          Validators.maxLength(20),
+          Validators.pattern('^[a-z0-9 ]*')
+        ]
+      ],
+      description: ['', 
+        [
+          Validators.required,
+          Validators.maxLength(100)
+        ]
+      ],
+      phoneno: ['',
+        [
+          Validators.required,
+          Validators.pattern('^[7][0-9]{9,10}$')
+        ]
+      ],
+      term: [false, 
+        [
+          Validators.requiredTrue
+        ]]
 
 
     })
 
   }
-  onSubmit(){
+  onSubmit() {
     console.log(this.registerForm.value)
   }
 
